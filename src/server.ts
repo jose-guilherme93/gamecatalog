87ccc6b2a0ad005fd6de5c4782b240f5aeccc4b9
jose-guilherme93 <jose-guilherme93@hotmail.com>
2025-11-28 10:27:14 -0300

    feat: enhance server security with new middlewares

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,8 @@ import reviewsRoutes from './routes/reviewsRoutes.js'
 import authRoutes from './routes/authRoutes.js'
 import { logger } from './scripts/logger.js'
 import { requestLogger } from './utils/middlewares.js'
-
+import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 const env = process.env.NODE_ENV || 'development'
 const envFile = `.env.${env}`
 dotenv.config({ path: path.resolve(process.cwd(), envFile), quiet: true })
@@ -16,12 +17,33 @@ dotenv.config({ path: path.resolve(process.cwd(), envFile), quiet: true })
 const PORT = Number(process.env.SERVER_PORT) || 3000
 
 const app = express()
+
+app.use(helmet())
+app.set('trust proxy', 1)
 app.use(cors())
 app.use(requestLogger)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true, limit: '10kb' }))
 
-app.use('/favicon.png', (req, res) => res.status(204).end())
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: JSON.stringify({ error: 'Too many requests, please try again later.' }),
+})
+
+const authLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: JSON.stringify({ error: 'Too many authentication attempts, please try again after an hour.' }),
+})
+
+app.use('/auth', authLimiter)
+app.use(globalLimiter)
+app.use('../favicon.png', (req, res) => res.status(204).end())
 app.get('/', (req, res) => res.status(200).json({
   message: 'Gamecatalog API',
   version: 'v0.1',
